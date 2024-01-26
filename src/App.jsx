60771df8314cedd72a79351b11313794ae0afa1f
whddbsl <whddbs04140@naver.com>
2024-01-26 20:43:36 +0900

    🚚: 라우터 설정 및 기본 폴더 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from "react";
 import {Routes, Route} from "react-router-dom";
+import React from "react";
 import MainPage from "./pages/MainPage";
 import AboutField from "./pages/AboutField";
 
